535ebbde40147742d2f4db94200edb78ba9ccc5f
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-06-30 15:53:39 +0100

    chore: set compileSDK to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,7 @@ plugins {
 
 android {
     namespace = "com.pedroid.android17locationbutton"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.pedroid.android17locationbutton"
